26249d691ab20af830622970bbea9187ffd09a7e
conquesticle <erhodes@GAIG.COM>
2020-05-16 13:24:58 -0400

    initial commit

---
+++ ForceLogoutUser.ps1
@@ -0,0 +1,15 @@
+﻿function Logout-User {
+    param(
+    [Parameter(Mandatory=$true,position=1)]
+    $username,
+
+    [Parameter(Mandatory=$false,position=2)]
+    $hostname = 'localhost'
+    )
+    begin{}
+    process{
+        $sessionId = ((quser /server:$hostname | ? {$_ -match $username}) -split ' +')[3]
+        logoff $sessionId /server:$hostname
+    }
+    end{}
+}
